59b4ac860db01f00c97fd4456554bf6a90660bc0
Mathieu Rio <mathieu.rio@epitech.eu>
2024-02-28 09:37:30 +0100

    fix: ajout d'un fix temp sur l'adresse du serveur de testg

--- Server/abimeUi/plugins/pocketbase.js
+++ Server/abimeUi/plugins/pocketbase.js
@@ -2,6 +2,6 @@
 import PocketBase from 'pocketbase';
 
 export default defineNuxtPlugin((nuxtApp) => {
-    const pb = new PocketBase('http://mathieu-rio.fr:8090');
+    const pb = new PocketBase('http://mathieu-rio.fr:8090'); // A fixer
     nuxtApp.provide('pb', pb);
 });
