e7c98cb96aaadc405e338ad9499e93f603c24994
Mathieu Rio <mathieu.rio@epitech.eu>
2024-02-28 09:30:31 +0100

    fix: addresse

--- Server/abimeUi/plugins/pocketbase.js
+++ Server/abimeUi/plugins/pocketbase.js
@@ -2,6 +2,6 @@
 import PocketBase from 'pocketbase';
 
 export default defineNuxtPlugin((nuxtApp) => {
-    const pb = new PocketBase('http://localhost:8090');
+    const pb = new PocketBase('http://mathieu-rio.fr:8090');
     nuxtApp.provide('pb', pb);
 });
